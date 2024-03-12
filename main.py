02ad16e848f201488fb2e8d6a843b068fbb35304
Adam Rodrigues <arodrigues@stgeorges.bc.ca>
2024-03-12 15:54:06 -0700

    added comments

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pulp as pl
 import pandas as pd
 
+RANKINGS_CSV_FILE_NAME = 'socials_rankings.csv'
+MAX_STUDENTS_PER_SESSION = 20
+NUMBER_OF_ROTATIONS = 3
 
 def main():
-    rankings = pd.read_csv('socials_rankings.csv')
+    """
+    This script takes a csv file in the current directory that contains as headers: a student identifier (eg. name, id), names of sessions, and
+    data which are integer rankings of the named sessions. The input file name, enrolment cap per session, and number of rotations can be adjusted.
+    Note: A student is assumed to be required to enrol in exactly one session per rotation, and may not enrol in the same session in multiple rotations.
+    """
 
-    num_students, num_sessions = rankings.shape[0], rankings.shape[1] - 1
+    rankings = pd.read_csv(RANKINGS_CSV_FILE_NAME)
+
+    num_students, num_sessions = rankings.shape[0], rankings.shape[1] - 1 # first column is student identifer, not session
     sessions = rankings.columns[1:]
-                      
-    MAX_STUDENTS_PER_SESSION = 20
-    NUMBER_OF_ROTATIONS = 3
-
+                          
     # Initialize the optimization model
     problem = pl.LpProblem("FlexDayAssignment", pl.LpMinimize)
 
@@ -42,7 +48,7 @@ def main():
             else:
                 problem += pl.lpSum(X[i, j, k] for i in range(num_students)) <= MAX_STUDENTS_PER_SESSION, f"cap_per_session_{j}_rotation_{k}"
     
-    # Create list of lists of rankings data (int)
+    # Create list of lists of int rankings from input data
     R = rankings.iloc[:, 1:].to_numpy().tolist()
 
     # Set the objective
@@ -51,9 +57,12 @@ def main():
     
     problem.solve()
 
+    # cataloguing for output
     session_enrolments = {k: {j: [] for j in range(num_sessions)} for k in range(NUMBER_OF_ROTATIONS)}
+    # for testing
     rankings_of_assigned_sessions_by_student = {i: [] for i in range(num_students)}
 
+    # parse solution variables
     for v in problem.variables():
         i, j, k = map(lambda x: int(x.replace('_', '')), v.name.split('(')[1].split(')')[0].split(','))
         x_ijk = v.value()
@@ -64,17 +73,19 @@ def main():
 
     # append nones so we can make the output dataframe
     max_length = max(len(lst) for lst in [session_enrolments[k][j] for j in range(num_sessions) for k in range(NUMBER_OF_ROTATIONS)])
-    
+
     for k in session_enrolments:
         for j in session_enrolments[k]:
             session_enrolments[k][j] += [None] * (max_length - len(session_enrolments[k][j]))
 
+    # create an output csv file for each rotation
     for k in range(NUMBER_OF_ROTATIONS):
         pd.DataFrame.from_dict(session_enrolments[k]).set_axis(sessions, axis='columns').to_csv(f"enrolments_for_rotation_{k}.csv", index=False)
 
     # check to see what students ranked their assigned sessions (measure for how well we did)
     for i, vals in rankings_of_assigned_sessions_by_student.items():
         print(rankings.iloc[i,0] + ":", vals)
+
 
 if __name__ == '__main__':
     main()
