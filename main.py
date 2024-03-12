f0b52e8cce280e39f057680e9fdd52e86fec4788
Adam Rodrigues <arodrigues@stgeorges.bc.ca>
2024-03-12 12:16:56 -0700

    working for multiple rounds

--- main.py
+++ main.py
@@ -6,53 +6,58 @@ def main():
     rankings = pd.read_csv('test.csv')
 
     num_students, num_sessions = rankings.shape[0], rankings.shape[1] - 1
+    sessions = rankings.columns[1:]
                       
     MAX_STUDENTS_PER_SESSION = 4
+    NUMBER_OF_ROTATIONS = 3
 
     # Initialize the optimization model
     model = gp.Model("FlexDayAssignment")
 
     # Define assignment matrix
-    X = model.addVars(num_students, num_sessions, vtype=gp.GRB.BINARY, name="assignment_matrix")
+    X = model.addVars(num_students, num_sessions, NUMBER_OF_ROTATIONS, vtype=gp.GRB.BINARY, name="assignment_matrix")
     
     # Specify contsraints
 
-    # Each student attends exactly one session
+    # Each student attends exactly one session per rotation
     for i in range(num_students):
-        model.addConstr(X.sum(i, '*') == 1, name=f"one_session_per_student_{i}")
+        for k in range(NUMBER_OF_ROTATIONS):
+            model.addConstr(sum(X[i, j, k] for j in range(num_sessions)) == 1, name=f"one_session_per_student_{i}_rotation_{k}")
 
-    # Enrollment cap per session
+    # Each student may attend the same session no more than once over all conferences
+    for i in range(num_students):
+        for j in range(num_sessions):
+            model.addConstr(sum(X[i, j, k] for k in range(NUMBER_OF_ROTATIONS)) <= 1, f"session_{j}_max_once_per_student_{i}")
+
+    # Enrollment cap per session for each rotation
     for j in range(num_sessions):
-        model.addConstr(X.sum('*', j) <= MAX_STUDENTS_PER_SESSION, name=f"cap_per_session_{j}")
+        for k in range(NUMBER_OF_ROTATIONS):
+            model.addConstr(sum(X[i, j, k] for i in range(num_students)) <= MAX_STUDENTS_PER_SESSION, name=f"cap_per_session_{j}_rotation_{k}")
 
+    # Create list of lists of rankings data (int)
     R = rankings.iloc[:, 1:].to_numpy().tolist()
 
-    objective = gp.quicksum((R[i][j] - 1)**2 * X[i, j] for i in range(num_students) for j in range(num_sessions))
+    objective = gp.quicksum((R[i][j] - 1)**2 * X[i, j, k] for i in range(num_students) for j in range(num_sessions) for k in range(NUMBER_OF_ROTATIONS))
 
     model.setObjective(objective, gp.GRB.MINIMIZE)
 
     model.optimize()
 
-    assignments = [(i, j) for i in range(num_students) for j in range(num_sessions) if X[i, j].X > 0.5]
-    assignments.sort()
-    print("Assignments:", assignments)
+    session_enrolments = {k: {j: [] for j in range(num_sessions)} for k in range(NUMBER_OF_ROTATIONS)}
 
-    sessions = rankings.columns[1:]
+    for (i, j, k), x_ijk in X.items():
+        if int(x_ijk.X) == 1:
+            session_enrolments[k][j].append(rankings.iloc[i,0])
 
-    session_enrolments = {j: [] for j in range(num_sessions)}
-
-    for (i, j), x_ij in X.items():
-        if int(x_ij.X):
-            session_enrolments[j].append(rankings.iloc[i,0])
-    
     # append nones so we can make the output dataframe
     max_length = max(len(lst) for lst in session_enrolments.values())
-    for j in session_enrolments:
-        session_enrolments[j] += [None] * (max_length - len(session_enrolments[j]))
+    
+    for k in session_enrolments:
+        for j in session_enrolments[k]:
+            session_enrolments[k][j] += [None] * (max_length - len(session_enrolments[k][j]))
 
-    assignments_df = pd.DataFrame(session_enrolments)
-    assignments_df.columns = sessions
-    assignments_df.to_csv('output.csv', index=False)
+    for k in range(NUMBER_OF_ROTATIONS):
+        pd.DataFrame.from_dict(session_enrolments[k]).set_axis(sessions, axis='columns').to_csv(f"enrolments_for_rotation_{k}.csv", index=False)
 
 
 if __name__ == '__main__':
